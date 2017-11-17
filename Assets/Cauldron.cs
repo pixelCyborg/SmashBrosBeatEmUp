6ecaa4a7247ed684712e8fc9686e5a672a448dc9
Harrison Cote <harrison.cote@gmail.com>
2017-11-17 15:42:47 -0800

    Starting cauldron work

---
+++ Assets/Cauldron.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Cauldron : MonoBehaviour {
+    public static Cauldron instance;
+    List<ItemTile> brewItems;
+    bool isHovering;
+
+    void Start()
+    {
+        instance = this;
+        brewItems = new List<ItemTile>();
+    }
+
+    public void AddItemToBrew(ItemTile item)
+    {
+        brewItems.Add(item);
+    }
+
+    public void RemoveFromBrew()
+    {
+
+    }
+
+    public void ClearBrew()
+    {
+
+    }
+}
